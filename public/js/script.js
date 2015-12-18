62abd70c2914e77734389f10416d9e2e6568f6ef
Erin Mahon <erin.mahon14@gmail.com>
2015-12-17 23:05:39 -0500

    Set up image slideshow on home.erb.

--- public/js/script.js
+++ public/js/script.js
@@ -27,4 +27,17 @@ $(document).click(function(){
     // });
     // $("#band").css("width", "40px");
     // $("#second-p").css("color", "red")
+
+$("#slideshow > div:gt(0)").hide();
+
+setInterval(function() { 
+  $('#slideshow > div:first')
+    .fadeOut(5000)
+    .next()
+    .fadeIn(5000)
+    // .delay(1000)
+    .end()
+    .appendTo('#slideshow');
+},  3000);
+
 });
